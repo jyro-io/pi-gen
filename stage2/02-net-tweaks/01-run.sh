0b17b4893e8c17ac5c4de0bea77b6e78195a2004
Rory Linehan <rorylinehan@protonmail.com>
2023-03-05 17:16:21 -0600

    rearrange

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -5,6 +5,12 @@ install -v -d "${ROOTFS_DIR}/etc/hostapd"
 install -v -m -b 600 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
 install -v -m -b 600 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 install -v -m -b 600 files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
+
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" brctl addbr br0
+	SUDO_USER="${FIRST_USER_NAME}" brctl addif br0 eth0
+EOF
+
 cat >> /etc/network/interfaces << EOL
 auto br0
 iface br0 inet manual
