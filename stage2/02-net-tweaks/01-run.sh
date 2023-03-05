0ddf3f531a8f4d492866e0d21424faa154504680
Rory Linehan <rorylinehan@protonmail.com>
2023-03-05 17:20:33 -0600

    more network config

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -e
 
 ## osss-monitor
+echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
 install -v -d "${ROOTFS_DIR}/etc/hostapd"
 install -v -m -b 600 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
 install -v -m -b 600 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 install -v -m -b 600 files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
+
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+	SUDO_USER="${FIRST_USER_NAME}" sh -c "iptables-save > /etc/iptables.ipv4.nat"
+EOF
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" brctl addbr br0
