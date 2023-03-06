10cff079cfacf840342dcca82bcf8e5884a51599
Rory Linehan <rorylinehan@protonmail.com>
2023-03-05 20:35:05 -0600

    -b -> --backup

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -5,9 +5,9 @@ echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 install -v -d "${ROOTFS_DIR}/etc/hostapd"
-install -v -m -b 600 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
-install -v -m -b 600 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
-install -v -m -b 600 files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
+install -v -m --backup 600 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
+install -v -m --backup 600 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
+install -v -m --backup 600 files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
