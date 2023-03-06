da29e853800fae5e7ca7ce0a6947dd478f346aeb
Rory Linehan <rorylinehan@protonmail.com>
2023-03-05 22:04:57 -0600

    don't need the bridge

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -14,15 +14,4 @@ on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 	SUDO_USER="${FIRST_USER_NAME}" sh -c "iptables-save > /etc/iptables.ipv4.nat"
 EOF
-
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" brctl addbr br0
-	SUDO_USER="${FIRST_USER_NAME}" brctl addif br0 eth0
-EOF
-
-cat >> /etc/network/interfaces << EOL
-auto br0
-iface br0 inet manual
-bridge_ports eth0 wlan0
-EOL
 ##
