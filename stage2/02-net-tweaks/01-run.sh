3a76c21616ccf988ac13923235e30c7348154c5e
Rory Linehan <rorylinehan@protonmail.com>
2023-03-05 22:06:34 -0600

    don't need iptables stuff

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -8,10 +8,4 @@ install -v -d "${ROOTFS_DIR}/etc/hostapd"
 install -v -m 600 -b files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
 install -v -m 600 -b files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 install -v -m 600 -b files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
-
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" update-alternatives --set iptables /usr/sbin/iptables-legacy
-	SUDO_USER="${FIRST_USER_NAME}" iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-	SUDO_USER="${FIRST_USER_NAME}" sh -c "iptables-save > /etc/iptables.ipv4.nat"
-EOF
 ##
