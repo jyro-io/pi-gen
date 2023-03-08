3411b921230d30af1f8058e78a7161c0279f6250
Rory Linehan <rorylinehan@protonmail.com>
2023-03-07 19:50:34 -0600

    add installer for app

---
+++ stage2/04-osss/00-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+install -v -d "${ROOTFS_DIR}/opt/osss"
+install -v -m 600 files/osss-camera "${ROOTFS_DIR}/opt/osss/"
+install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
+install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
+
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" systemctl daemon-reload
+	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
+EOF
