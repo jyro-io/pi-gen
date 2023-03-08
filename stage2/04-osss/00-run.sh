4d7eb2cf1ff2e662bb33f131b09581e620f0c8d8
Rory Linehan <rorylinehan@protonmail.com>
2023-03-07 19:59:57 -0600

    remove unnecessary sudo user

--- stage2/04-osss/00-run.sh
+++ stage2/04-osss/00-run.sh
@@ -6,6 +6,6 @@ install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" systemctl daemon-reload
-	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
+	systemctl daemon-reload
+	systemctl enable osss-monitor
 EOF
