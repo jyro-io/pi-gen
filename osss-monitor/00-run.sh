9753acca9ea220af680e2c95704342ad31b6af3d
Rory Linehan <rorylinehan@protonmail.com>
2023-03-11 21:38:12 -0600

    use stage list build parameter

--- osss-monitor/00-run.sh
+++ osss-monitor/00-run.sh
@@ -6,5 +6,6 @@ install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 on_chroot << EOF
+	systemctl daemon-reload
 	systemctl enable osss-monitor
 EOF
