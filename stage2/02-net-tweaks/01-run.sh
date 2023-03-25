4b095d84b873c8a2175df965359d6cba51d1efda
Rory Linehan <rorylinehan@protonmail.com>
2023-03-25 10:17:40 -0500

    config updates, remove bridge stuff

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -8,13 +8,10 @@ install -v -m 600 -b files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
 
 on_chroot << EOF
 	echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
-	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable dhcpcd
-	SUDO_USER="${FIRST_USER_NAME}" systemctl enable dnsmasq
-
-	SUDO_USER="${FIRST_USER_NAME}" sed -i -z 's/\[Service\]\n/\[Service\]\nExecStartPre=\/bin\/sleep 15\n/' /lib/systemd/system/hostapd.service
-	SUDO_USER="${FIRST_USER_NAME}" systemctl daemon-reload
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable hostapd
+	SUDO_USER="${FIRST_USER_NAME}" systemctl enable dnsmasq
+	SUDO_USER="${FIRST_USER_NAME}" systemctl daemon-reload
 EOF
 ##
