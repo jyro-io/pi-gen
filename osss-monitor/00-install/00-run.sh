e4a1d1eb4c218b49e701835116550eb277328a93
Rory Linehan <rorylinehan@protonmail.com>
2023-03-25 13:31:22 -0500

    add chown for config dir

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -4,9 +4,11 @@ install -v -d "${ROOTFS_DIR}/opt/osss"
 install -v -m 600 files/osss-monitor "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
+
 mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
 install -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-monitor
+	SUDO_USER="${FIRST_USER_NAME}" chown -Rv ${FIRST_USER_NAME} ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config
 EOF
