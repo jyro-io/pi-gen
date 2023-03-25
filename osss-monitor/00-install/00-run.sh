7745d771deb5a8cb573daf48f68f93fe049291de
Rory Linehan <rorylinehan@protonmail.com>
2023-03-25 14:27:36 -0500

    autostart tweaks

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -5,10 +5,9 @@ install -v -m 600 files/osss-monitor "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
 
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
-install -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
-
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-monitor
-	SUDO_USER="${FIRST_USER_NAME}" chown -Rv ${FIRST_USER_NAME} ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config
+	SUDO_USER="${FIRST_USER_NAME}" mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
 EOF
+
+install -o ${FIRST_USER_NAME} -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
