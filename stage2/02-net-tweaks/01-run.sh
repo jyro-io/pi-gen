9923ac461f14cc71b5ea09c0dfcf3930e36541d4
Rory Linehan <rorylinehan@protonmail.com>
2023-03-24 19:11:04 -0500

    wifi access point tweaks

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -e
 
 ## osss-monitor
-echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-
 install -v -d "${ROOTFS_DIR}/etc/hostapd"
 install -v -m 600 -b files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
 install -v -m 600 -b files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 install -v -m 600 -b files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
+
+on_chroot << EOF
+	echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
+	echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+	
+	systemctl enable dhcpcd
+	systemctl enable dnsmasq
+	systemctl enable hostapd
+EOF
 ##
