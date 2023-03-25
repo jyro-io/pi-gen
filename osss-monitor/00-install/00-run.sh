3fdbb03dccb7797250689b1a39fbd87be611d52b
Rory Linehan <rorylinehan@protonmail.com>
2023-03-25 11:50:36 -0500

    add mkdir for autostart

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -4,6 +4,7 @@ install -v -d "${ROOTFS_DIR}/opt/osss"
 install -v -m 600 files/osss-monitor "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
+mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
 install -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
 
 on_chroot << EOF
