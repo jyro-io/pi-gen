d7baad08c82de620503229b5d52367c32a6438e7
Rory Linehan <rorylinehan@protonmail.com>
2023-03-25 11:10:38 -0500

    add camera stream file to autostart directory

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -4,7 +4,7 @@ install -v -d "${ROOTFS_DIR}/opt/osss"
 install -v -m 600 files/osss-monitor "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
-install -v -m 600 files/camera-stream.service "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Desktop/"
+install -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-monitor
