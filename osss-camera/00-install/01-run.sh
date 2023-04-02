cfac342e4a300880319f4211ba4d5609a892acd5
Rory Linehan <rorylinehan@protonmail.com>
2023-04-01 21:57:34 -0500

    changes for motion config

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -5,6 +5,11 @@ install -v -m 600 files/osss-camera "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
 
+install -v -d "${ROOTFS_DIR}/var/log/motion"
+
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
+	SUDO_USER="${FIRST_USER_NAME}" chown -Rv motion:motion /var/log/motion
+	SUDO_USER="${FIRST_USER_NAME}" systemctl enable motion
 EOF
+
