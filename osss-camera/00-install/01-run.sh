72be0af14c6d77e090bf7490007e04c338bee95c
Rory Linehan <rorylinehan@protonmail.com>
2023-04-01 23:32:11 -0500

    add motion config file install

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -5,7 +5,9 @@ install -v -m 600 files/osss-camera "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
 
+install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/videos"
 install -v -d "${ROOTFS_DIR}/var/log/motion"
+install -v -m 600 files/motion.conf "${ROOTFS_DIR}/usr/local/etc/motion/"
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
