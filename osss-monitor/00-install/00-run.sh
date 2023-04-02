181c6815366816932c1b6288ae74bd8f0a88dc57
Rory Linehan <rorylinehan@protonmail.com>
2023-04-02 16:12:43 -0500

    monitor build working correctly

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -5,9 +5,10 @@ install -v -m 600 files/osss-monitor "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
 
+install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
+install -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
+
 on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" chown -Rv ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/.config
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-monitor
 EOF
-
-install -o ${FIRST_USER_NAME} -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
-install -o ${FIRST_USER_NAME} -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
