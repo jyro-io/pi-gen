77b653d174746d49f6cfeb7fab6c24ab03de1ff8
Rory Linehan <rorylinehan@protonmail.com>
2023-04-08 16:47:03 -0500

    adding libcamera-vid service

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -4,6 +4,7 @@ install -v -d "${ROOTFS_DIR}/usr/local/etc/motion/"
 install -v -d "${ROOTFS_DIR}/opt/osss"
 install -v -m 600 files/osss-camera "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
+install -v -m 600 files/libcamera-vid.service "${ROOTFS_DIR}/lib/systemd/system/"
 install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/videos"
@@ -14,7 +15,7 @@ install -v -m 600 files/motion.conf "${ROOTFS_DIR}/etc/motion/"
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-camera
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
-	SUDO_USER="${FIRST_USER_NAME}" chown -Rv motion:motion /var/log/motion
+	SUDO_USER="${FIRST_USER_NAME}" chown -Rv motion:motion /var/log/motion /home/${FIRST_USER_NAME}/videos /etc/motion
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable motion
 EOF
 
