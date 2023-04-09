dc9e2438e934649feb45584f00731b93d6b1d227
Rory Linehan <rorylinehan@protonmail.com>
2023-04-08 21:25:30 -0500

    drop libcamera-vid service

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -4,7 +4,6 @@ install -v -d "${ROOTFS_DIR}/usr/local/etc/motion/"
 install -v -d "${ROOTFS_DIR}/opt/osss"
 install -v -m 600 files/osss-camera "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
-install -v -m 600 files/libcamera-vid.service "${ROOTFS_DIR}/lib/systemd/system/"
 install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
 install -v -m 600 files/motion.service "${ROOTFS_DIR}/lib/systemd/system/"
 
@@ -15,7 +14,6 @@ install -v -m 600 files/motion.conf "${ROOTFS_DIR}/etc/motion/"
 chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-camera
 
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" systemctl disable libcamera-vid
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable motion
 	chown -Rv motion:motion /var/log/motion /home/${FIRST_USER_NAME}/videos /etc/motion
