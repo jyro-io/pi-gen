6066eb8d5c23d752aed304aeb8843398cf225ee6
Rory Linehan <rorylinehan@protonmail.com>
2023-04-08 19:09:43 -0500

    fixes

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -12,10 +12,12 @@ install -v -d "${ROOTFS_DIR}/var/log/motion"
 #install -v -m 600 files/motion.conf "${ROOTFS_DIR}/usr/local/etc/motion/"
 install -v -m 600 files/motion.conf "${ROOTFS_DIR}/etc/motion/"
 
+chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-camera
+chown -Rv motion:motion /var/log/motion /home/${FIRST_USER_NAME}/videos /etc/motion
+
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-camera
+	SUDO_USER="${FIRST_USER_NAME}" systemctl enable libcamera-vid
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
-	SUDO_USER="${FIRST_USER_NAME}" chown -Rv motion:motion /var/log/motion /home/${FIRST_USER_NAME}/videos /etc/motion
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable motion
 EOF
 
