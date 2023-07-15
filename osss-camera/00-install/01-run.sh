7f172942e6677d870ee1806dd8a9cfd5620530db
Rory Linehan <rorylinehan@protonmail.com>
2023-07-15 16:25:16 -0500

    update for gocv

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -e
 
 install -v -d "${ROOTFS_DIR}/opt/osss"
-install -v -m 600 files/osss-camera "${ROOTFS_DIR}/opt/osss/"
+
+# install gocv
+cd "${ROOTFS_DIR}/opt/osss" && \
+git clone https://github.com/hybridgroup/gocv.git && \
+cd gocv && \
+make install_raspi
+
+# build osss-camera
+install -v -m 600 files/go.mod "${ROOTFS_DIR}/opt/osss/" && \
+install -v -m 600 files/main.go "${ROOTFS_DIR}/opt/osss/" && \
+go mod tidy && \
+env GOOS=linux GOARCH=arm GOARM=5 go build -o osss-camera . && \
+chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-camera
+
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
-
-chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-camera
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-camera
