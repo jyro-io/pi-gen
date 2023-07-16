eeeeff0dec103f8ac202675246b4feca180f4def
Rory Linehan <rorylinehan@protonmail.com>
2023-07-16 12:06:17 -0500

    changes for gocv build/install

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -e
 
 install -v -d "${ROOTFS_DIR}/opt/osss"
-install -v -m 600 files/osss-monitor "${ROOTFS_DIR}/opt/osss/"
+install -v -m 600 files/go.mod "${ROOTFS_DIR}/opt/osss/"
+install -v -m 600 files/go.sum "${ROOTFS_DIR}/opt/osss/"
+install -v -m 600 files/main.go "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
 
-install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
-install -v -m 600 files/camera-stream.desktop "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/autostart/"
-
-chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-monitor
-
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" chown -Rv ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/.config
+  # install gocv
+  SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss
+  if [ ! -d "/opt/osss/gocv" ]; then
+    git clone --branch release https://github.com/hybridgroup/gocv.git
+  fi
+  cd gocv && \
+  git checkout --force 7811007fdb9f8ea1a32db0913846361be05d4973 && \
+  make -j 4 install_raspi
+
+  # build osss-monitor
+  SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss/ && \
+  go mod edit -go=1.15 && \
+  go mod tidy && \
+  env GOOS=linux GOARCH=arm GOARM=5 go build -o osss-monitor .
+
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-monitor
 EOF
+
+chmod -v +x ${ROOTFS_DIR}/opt/osss/osss-monitor
