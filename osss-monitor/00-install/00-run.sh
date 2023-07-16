0373fb8f77d422317e47514de9c98312516dd306
Rory Linehan <rorylinehan@protonmail.com>
2023-07-16 12:07:25 -0500

    spaces -> tabs

--- osss-monitor/00-install/00-run.sh
+++ osss-monitor/00-install/00-run.sh
@@ -8,20 +8,20 @@ install -v -m 600 files/config.yaml "${ROOTFS_DIR}/opt/osss/"
 install -v -m 600 files/osss-monitor.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 on_chroot << EOF
-  # install gocv
-  SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss
-  if [ ! -d "/opt/osss/gocv" ]; then
-    git clone --branch release https://github.com/hybridgroup/gocv.git
-  fi
-  cd gocv && \
-  git checkout --force 7811007fdb9f8ea1a32db0913846361be05d4973 && \
-  make -j 4 install_raspi
+	# install gocv
+	SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss
+	if [ ! -d "/opt/osss/gocv" ]; then
+		git clone --branch release https://github.com/hybridgroup/gocv.git
+	fi
+	cd gocv && \
+	git checkout --force 7811007fdb9f8ea1a32db0913846361be05d4973 && \
+	make -j 4 install_raspi
 
-  # build osss-monitor
-  SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss/ && \
-  go mod edit -go=1.15 && \
-  go mod tidy && \
-  env GOOS=linux GOARCH=arm GOARM=5 go build -o osss-monitor .
+	# build osss-monitor
+	SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss/ && \
+	go mod edit -go=1.15 && \
+	go mod tidy && \
+	env GOOS=linux GOARCH=arm GOARM=5 go build -o osss-monitor .
 
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable osss-monitor
 EOF
