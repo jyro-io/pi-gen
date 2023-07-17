e9474ffa29b9ec4c8eb0e9fa2fea0c51871c2a26
Rory Linehan <rorylinehan@protonmail.com>
2023-07-16 22:57:42 -0500

    sprinkle sudo elsewhere

--- osss-camera/00-install/01-run.sh
+++ osss-camera/00-install/01-run.sh
@@ -9,13 +9,13 @@ install -v -m 600 files/osss-camera.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 on_chroot << EOF
 	# install gocv
-	SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss
+	cd /opt/osss
 	if [ ! -d "/opt/osss/gocv" ]; then
 		git clone --branch release https://github.com/hybridgroup/gocv.git
 	fi
 	cd gocv && \
 	git checkout --force 7811007fdb9f8ea1a32db0913846361be05d4973 && \
-	make -j 4 install_raspi_zero
+	SUDO_USER="${FIRST_USER_NAME}" make -j 4 install_raspi_zero
 
 	# build osss-camera
 	SUDO_USER="${FIRST_USER_NAME}" cd /opt/osss/ && \
